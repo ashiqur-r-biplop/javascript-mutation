517b9e892a9f5d10d48dce6b570707a97eb4ce7d
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-25 14:03:56 +0600

    first commit

---
+++ mutation.js
@@ -0,0 +1,12 @@
+let demonSlayers = [
+  "Tanjiro",
+  "Rengoku",
+  "Tengen",
+  "Opennai",
+  "Mitsuri",
+  "Shinobu",
+];
+
+// demonSlayers.push("Zenetsu");
+demonSlayers =  ['Zenetsu', "Inoske", ...demonSlayers]
+console.log(demonSlayers);
